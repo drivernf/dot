aa674a347e1fa1a9c5611767276f2698d996bf40
Nick Driver <drivernf99@gmail.com>
2026-03-07 09:41:09 -0500

    Adds ghostty and various tweaks

--- hypr/autostart-inet.sh
+++ hypr/autostart-inet.sh
@@ -2,20 +2,12 @@
 nm-online -q --timeout=30
 
 hyprctl dispatch workspace 5
-hyprctl dispatch exec '[workspace 5 silent] google-chrome-stable --profile-directory="Profile 1"'
+hyprctl dispatch exec '[workspace 5] google-chrome-stable --profile-directory="Profile 1"'
 
 sleep 5s
-
-hyprctl dispatch workspace 4
-hyprctl dispatch exec '[workspace 4 silent] discord'
-
-sleep 5s
-
-hyprctl dispatch workspace 3
-hyprctl dispatch exec '[workspace 3 silent] code'
 
 hyprctl dispatch workspace 2
-hyprctl dispatch exec '[workspace 2 silent] ghostty'
+hyprctl dispatch exec '[workspace 2] ghostty'
 
 hyprctl dispatch workspace 1
-hyprctl dispatch exec '[workspace 1 silent] google-chrome-stable --profile-directory="Default"'
+hyprctl dispatch exec '[workspace 1] google-chrome-stable --profile-directory="Default"'
